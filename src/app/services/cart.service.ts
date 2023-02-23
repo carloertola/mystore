1c11e7bb0d1e25af167d3010a626bd6a6e414a2a
carloertola <carloertola@gmail.com>
2023-02-24 01:11:09 +0300

    added a new cart service to handle tasks of adding cart items, getting cart items, and clearing cart items

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+  items: Product[] = [];
+
   constructor() { }
+
+  addToCart(product: Product) {
+    let existingProduct = this.items.find(({ id }) => id === product.id);
+    if(existingProduct === undefined) {
+      this.items.push(product);
+      window.alert("Product successfully added to Cart!")
+    } else {
+      existingProduct.quantity += product.quantity;
+      window.alert("We've added more of this item!")
+    }
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
