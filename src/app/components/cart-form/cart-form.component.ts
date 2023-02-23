21421533e90c142c479bf2b64fb4bd206f073254
carloertola <carloertola@gmail.com>
2023-02-24 01:09:31 +0300

    worked on cart form to try and fix the quantity issues and linked it to cart service

--- src/app/components/cart-form/cart-form.component.ts
+++ src/app/components/cart-form/cart-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-cart-form',
@@ -8,5 +10,15 @@ import { Product } from 'src/app/models/product';
 })
 export class CartFormComponent {
   options: number[] = [1, 2, 3, 4, 5, 10, 20, 30, 50, 100];
+  quantityInput = new FormControl('1');
+  quantity = Number(this.quantityInput);
+
   @Input() product!: Product;
+
+  constructor(private cartService: CartService) { }
+
+  addToCart(product: Product) {
+    product.quantity = this.quantity;
+    this.cartService.addToCart(product);
+  }
 }
