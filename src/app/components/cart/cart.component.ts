2a6a690d4f928acc80bfad0ef0e39a957bf61c61
carloertola <carloertola@gmail.com>
2023-02-24 01:06:56 +0300

    added cart component and set up view and logic

---
+++ src/app/components/cart/cart.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent {
+  items: Product[] = this.cartService.getItems();
+
+  constructor(private cartService: CartService) { }
+
+
+}
