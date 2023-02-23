fd0a5f615e2a1a7c0b8a670438a6de8f97a487c0
carloertola <carloertola@gmail.com>
2023-02-24 01:08:23 +0300

    added new modules to try reactive forms in passing the quantity values from user forms to cart orders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +10,7 @@ import { HeaderComponent } from './layout/header/header.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CartFormComponent } from './components/cart-form/cart-form.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +18,15 @@ import { ProductDetailsComponent } from './components/product-details/product-de
     HeaderComponent,
     ProductListComponent,
     CartFormComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
