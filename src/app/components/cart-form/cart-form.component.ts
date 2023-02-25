94d9d4b503fc9840c7e8312b91c52f7b0318d271
carloertola <carloertola@gmail.com>
2023-02-26 00:45:09 +0300

    binded the form to the quantity input using a better method

--- src/app/components/cart-form/cart-form.component.ts
+++ src/app/components/cart-form/cart-form.component.ts
@@ -1,24 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { CartService } from 'src/app/services/cart.service';
-import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-cart-form',
   templateUrl: './cart-form.component.html',
   styleUrls: ['./cart-form.component.css']
 })
-export class CartFormComponent {
-  options: number[] = [1, 2, 3, 4, 5, 10, 20, 30, 50, 100];
-  quantityInput = new FormControl('1');
-  quantity = Number(this.quantityInput);
+export class CartFormComponent implements OnInit {
 
   @Input() product!: Product;
 
   constructor(private cartService: CartService) { }
 
-  addToCart(product: Product) {
-    product.quantity = this.quantity;
+  ngOnInit(): void {
+  }
+
+  addToCart(product: Product, qty: string) {
+    let quantity = Number(qty);
+    product.quantity = quantity;
     this.cartService.addToCart(product);
   }
 }
