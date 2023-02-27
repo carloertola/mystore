3af96ac78878e1eb8a1b4eba1755fe14b1edc483
carloertola <carloertola@gmail.com>
2023-02-28 00:11:38 +0300

    added new route for order confirmation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component';
+import { OrderConfirmationComponent } from './components/order-confirmation/order-confirmation.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', component: ProductListComponent },
   { path: 'products/:productId', component: ProductDetailsComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'orderconfirmed',  component: OrderConfirmationComponent },
   { path: '**', redirectTo: '/' }
 ];
 
